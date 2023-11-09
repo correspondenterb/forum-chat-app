dd5ab2c2c5acd0bd0b771e17aa1eb519ede47f55
Rafael Bianchi <correspondenterb@gmail.com>
2023-11-08 21:00:56 -0300

    add cors to websocket

--- src/main/java/com/example/chatforumapp/config/WebSocketConfig.java
+++ src/main/java/com/example/chatforumapp/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.example.chatforumapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,7 +21,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket");
+        registry
+                .addEndpoint("/gs-guide-websocket")
+                .setAllowedOrigins("*");
     }
 
 }
